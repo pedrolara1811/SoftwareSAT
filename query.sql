aa612ae4a85c5f6dcd00b56d1ddb51972e807a6a
pedrolara1811 <125419090+pedrolara1811@users.noreply.github.com>
2024-11-24 12:58:52 -0600

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,13 @@
 create database validation_db;ç
 use validation_db;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rfc VARCHAR(13) NOT NULL,
+    cer_path VARCHAR(255) NOT NULL,
+    key_path VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 CREATE TABLE certificates (
          id INT AUTO_INCREMENT PRIMARY KEY,
          rfc VARCHAR(13) NOT NULL,
